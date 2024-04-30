6b7c51f685451db3650eaa8aafc847091b10806f
Flávio <f.augustosdn@gmail.com>
2024-04-30 11:12:05 -0300

    criado aplicativo de contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(color: Colors.purple));
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.orange), home: Homepage()));
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  var _count = 0;
+
+  void increment() {
+    _count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu primeiro app Flutter"),
+      ),
+      body: Center(
+          child: Text(
+        "Contador\n$_count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
